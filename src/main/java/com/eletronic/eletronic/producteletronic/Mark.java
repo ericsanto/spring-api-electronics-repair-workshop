7acbc79c665193e49d30e177ba11e42219a0c274
ericsanto <ericjesus403@gmail.com>
2025-01-18 16:18:33 -0300

    feat: Create enum for attribute mark of the class ProductEletronicEntity

---
+++ src/main/java/com/eletronic/eletronic/producteletronic/Mark.java
@@ -0,0 +1,16 @@
+package com.eletronic.eletronic.producteletronic;
+
+public enum Mark {
+
+    PHILIPS,
+    CCE,
+    AOC,
+    ACER,
+    SAMSUNG,
+    TCL,
+    PANASONIC,
+    LENOX,
+    CENTURY,
+    SONY,
+    LG
+}
