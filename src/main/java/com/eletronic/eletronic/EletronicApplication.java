debb8339e8ab3ad9fbb269a896a077ceed5efa13
ericsanto <ericjesus403@gmail.com>
2025-01-18 16:15:39 -0300

    feat: Core of the application

---
+++ src/main/java/com/eletronic/eletronic/EletronicApplication.java
@@ -0,0 +1,13 @@
+package com.eletronic.eletronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicApplication.class, args);
+	}
+
+}
