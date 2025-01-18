bb01321210c9b26704f5fb33987a297ef4436899
ericsanto <ericjesus403@gmail.com>
2025-01-18 16:21:42 -0300

    feat: Create repository ProductEletronicRepository for interact database column product_eletronic

---
+++ src/main/java/com/eletronic/eletronic/repository/ProductEletronicRepository.java
@@ -0,0 +1,7 @@
+package com.eletronic.eletronic.repository;
+
+import com.eletronic.eletronic.producteletronic.ProductEletronicEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductEletronicRepository extends JpaRepository<ProductEletronicEntity, Long> {
+}
