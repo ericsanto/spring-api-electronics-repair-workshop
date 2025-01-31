e0c1bad891ff63ca1a4f1c72d5abb4a0e1bfa6bf
ericsanto <ericjesus403@gmail.com>
2025-01-31 19:46:37 -0300

    feat: Create repository for SpareParts

--- src/main/java/com/eletronic/eletronic/models/spareparts/SparePartsEntity.java
+++ src/main/java/com/eletronic/eletronic/models/spareparts/SparePartsEntity.java
@@ -1,4 +1,57 @@
 package com.eletronic.eletronic.models.spareparts;
 
+
+import com.eletronic.eletronic.order.service.part.OrderServicePart;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name= "spare_part")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SparePartsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Column(name = "model")
+    @NotNull
+    @NotBlank
+    private String model;
+
+    @Column(name = "unit_price")
+    @NotNull
+    private Integer unitPrice;
+
+    @Column(name = "stock_quantity")
+    @NotNull
+    private Integer stockQuantity;
+
+    @OneToMany(mappedBy = "sparePart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderServicePart> orderServicePart = new ArrayList<>();
+
+
+
+
+
 }
