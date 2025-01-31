e962fa534b010ecefc545b1afd8cb1dd7b93537b
ericsanto <ericjesus403@gmail.com>
2025-01-31 19:43:31 -0300

    feat: Create service for SparePart

--- src/main/java/com/eletronic/eletronic/repository/SparePartRepository.java
+++ src/main/java/com/eletronic/eletronic/repository/SparePartRepository.java
@@ -1,4 +1,10 @@
 package com.eletronic.eletronic.repository;
 
-public interface SparePartRepository {
+import com.eletronic.eletronic.models.spareparts.SparePartsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SparePartRepository extends JpaRepository<SparePartsEntity, Long> {
+
 }
