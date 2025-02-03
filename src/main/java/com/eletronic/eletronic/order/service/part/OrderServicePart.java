d82f0405abede0547f06ff66c4601b42e1bf7507
ericsanto <ericjesus403@gmail.com>
2025-02-03 12:10:50 -0300

    feat: create service OrderServicePart

--- src/main/java/com/eletronic/eletronic/order/service/part/OrderServicePart.java
+++ src/main/java/com/eletronic/eletronic/order/service/part/OrderServicePart.java
@@ -1,4 +1,38 @@
 package com.eletronic.eletronic.order.service.part;
 
+
+import com.eletronic.eletronic.models.spareparts.SparePartsEntity;
+import com.eletronic.eletronic.service.ProductEletronicService;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_service_part")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class OrderServicePart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_service_id", nullable = false)
+    private ProductEletronicService.OrderService orderService;
+
+    @ManyToOne
+    @JoinColumn(name = "spare_part_id", nullable = false)
+    private SparePartsEntity sparePart;
+
+    @Column(name = "quantity")
+    @Positive
+    private Integer quantity;
+
 }
