0c515a697b2411aa9e4422c58516242f6f035024
ericsanto <ericjesus403@gmail.com>
2025-02-03 12:13:32 -0300

    feat: create service OrderServiceService

--- src/main/java/com/eletronic/eletronic/service/OrderServiceService.java
+++ src/main/java/com/eletronic/eletronic/service/OrderServiceService.java
@@ -1,4 +1,50 @@
 package com.eletronic.eletronic.service;
 
+import com.eletronic.eletronic.repository.OrderServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class OrderServiceService {
+
+    @Autowired
+    private OrderServiceRepository repository;
+
+
+
+    public List<ProductEletronicService.OrderService> getAllOrderService() {
+
+        List<ProductEletronicService.OrderService> orderServices = this.repository.findAll();
+
+        return orderServices;
+    }
+
+    public ProductEletronicService.OrderService getOrderServiceFindById(Long id) {
+
+        Optional<ProductEletronicService.OrderService> orderService = this.repository.findById(id);
+
+        return orderService.orElseThrow(() -> new RuntimeException("Does not exist order service with id"));
+    }
+
+    public ProductEletronicService.OrderService postOrderService(ProductEletronicService.OrderService data) {
+
+        this.repository.save(data);
+
+        return data;
+    }
+
+    public ProductEletronicService.OrderService putOrderService(ProductEletronicService.OrderService data, Long id) {
+        ProductEletronicService.OrderService orderService = this.getOrderServiceFindById(id);
+
+        orderService.setDescription(data.getDescription());
+        orderService.setOrderServicePart(data.getOrderServicePart());
+        orderService.setDateClose(data.getDateClose());
+
+        this.repository.save(orderService);
+
+        return orderService;
+
+    }
+
 }
